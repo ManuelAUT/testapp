d95e74af43950167a38f344eaf012d799a93ba5e
ManuelAUT <51486213+ManuelAUT@users.noreply.github.com>
2021-03-16 14:09:11 +0100

    db change

--- createdb.php
+++ createdb.php
@@ -1,26 +1,29 @@
 <?php
 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:direktdb.database.windows.net,1433; Database = Products", "student", "asdf1234.");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "CREATE TABLE Products(
-		`id` INT NOT NULL AUTO_INCREMENT ,
-		`bezeichnung` VARCHAR(30) NOT NULL ,
-		`langbeschreibung` VARCHAR(256) NOT NULL ,
-		`thumbnail` VARCHAR(256) NOT NULL ,
-		`reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+    $host = 'direktdb.database.windows.net';
+    $username = 'student';
+    $password = 'asdf1234.';
+    $db_name = 'Products';
+
+    //Establishes the connection
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 1433);
+    if (mysqli_connect_errno($conn)) {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }
+        
+    // Run the create table query
+    if (mysqli_query($conn, '
+        CREATE TABLE Products (
+        `id` INT NOT NULL AUTO_INCREMENT ,
+        `bezeichnung` VARCHAR(30) NOT NULL ,
+        `langbeschreibung ` VARCHAR(256) NOT NULL ,
+        `thumbnail` VARCHAR(256) NOT NULL ,
+        `reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
         PRIMARY KEY (`Id`)
-	)";
-
-	$conn->exec($sql);
-	echo "Table Products created successfully" . "\n";
-	}
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage() . "\n";
-    }
+        );
+    '))
+    
+    //Close the connection
+    mysqli_close($conn);
 ?>
-<br>
-<br>
-<a href=./index.php>Back</a>
-
