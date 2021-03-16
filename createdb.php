0d6bdfd47d9fc301872e87241e1bf6627aae275f
ManuelAUT <51486213+ManuelAUT@users.noreply.github.com>
2021-03-16 13:58:56 +0100

    db change

--- createdb.php
+++ createdb.php
@@ -4,11 +4,12 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:direktdb.database.windows.net,1433; Database = Products", "student", "asdf1234.");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql = "CREATE TABLE Products(
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		bezeichnung VARCHAR(30) NOT NULL,
-		langbeschreibung VARCHAR(256) NOT NULL,
-		thumbnail VARCHAR(256) NOT NULL,
-		reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+		`id` INT NOT NULL AUTO_INCREMENT ,
+		`bezeichnung` VARCHAR(30) NOT NULL ,
+		`langbeschreibung` VARCHAR(256) NOT NULL ,
+		`thumbnail` VARCHAR(256) NOT NULL ,
+		`reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+        PRIMARY KEY (`Id`)
 	)";
 
 	$conn->exec($sql);
