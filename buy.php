1bc83310585b35a56039a1111fc797501a7741d6
ManuelAUT <51486213+ManuelAUT@users.noreply.github.com>
2021-06-10 18:06:10 +0200

    update bus

--- buy.php
+++ buy.php
@@ -17,36 +17,38 @@ try {
 	$id = $_POST['artnr'];
 	$menge = $_POST['menge'];
 	$comment = $_POST['comment'];
-
+  echo "1";
 }
 catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage() . "\n";
     }
 
+    echo "2";
     $connectionString = "Endpoint=sb://direktsms.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=lir3CwExjuUrE2iVpkx+HNgL4OSx8ykhMeaMQxTs84o=";
     // Create Service Bus REST proxy.
      $serviceBusRestProxy = ServicesBuilder::getInstance()->createServiceBusService($connectionString);
-
+     echo "3";
 try    {
-     echo "create msg";
+     echo "create msg <br>";
      // Create message.
      $message = new BrokeredMessage();
      $message->setBody("message");
      echo "send msg";
-     echo "ArtNr: " . "$id" . ", Menge: " . "$menge" . ", Comment :" . "$comment";
+     echo "ArtNr: " . "$id" . ", Menge: " . "$menge" . ", Comment :" . "$comment" . "<br>";
      // Send message.
      $serviceBusRestProxy->sendQueueMessage("buymsg", $message);
-     echo "SMS";
+     echo "SMS sent";
  }
  catch(ServiceException $e){
-     echo "catch";
+     echo "4";
      $code = $e->getCode();
      $error_message = $e->getMessage();
      echo $code.": ".$error_message."<br />";
-     echo "catch";
+     echo "5";
  }
 
+echo "6";
 header("Location: ./index.php");
 }
 ?>
